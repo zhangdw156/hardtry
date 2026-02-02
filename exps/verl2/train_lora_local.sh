8afd051ea37ce20fbbe4dcf767d5dd60aeb8977b
zhangdw <zhangdw.cs@gmail.com>
2026-02-02 22:53:05 +0800

    feat: split verl1 into verl1 and verl2

--- exps/verl2/train_lora_local.sh
+++ exps/verl2/train_lora_local.sh
@@ -4,9 +4,9 @@ SCRIPT_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") &>/dev/null && pwd)"
 echo "SCRIPT_DIR: ${SCRIPT_DIR}"
 
 PYTHON_CMD="/dfs/data/uv-venv/verl/bin/python3"
-CONFIG_PATH="/dfs/data/work/hardtry/exps/verl1/configs"
-CONFIG_NAME="grpo_config"
-LOG_PATH="${SCRIPT_DIR}/logs/grpo.log"
+CONFIG_PATH="/dfs/data/work/hardtry/exps/verl2/configs"
+CONFIG_NAME="grpo_lora_config"
+LOG_PATH="${SCRIPT_DIR}/logs/grpo_lora.log"
 
 ${PYTHON_CMD} -m verl.trainer.main_ppo \
     --config-path=${CONFIG_PATH} \
