8b305b06a32d41e17ae2a4296f0548bed45b3922
zhangdw <zhangdw.cs@gmail.com>
2026-02-02 20:28:48 +0800

    feat: improve verl1 exp

--- exps/verl1/train_local.sh
+++ exps/verl1/train_local.sh
@@ -3,11 +3,7 @@
 SCRIPT_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") &>/dev/null && pwd)"
 echo "SCRIPT_DIR: ${SCRIPT_DIR}"
 
-if [ -n "$USE_LOCAL_VERL" ]; then
-    PYTHON_CMD="/dfs/data/uv-venv/verl/bin/python3"
-else
-    PYTHON_CMD="python3"
-fi
+PYTHON_CMD="/dfs/data/uv-venv/verl/bin/python3"
 
 ${PYTHON_CMD} -m verl.trainer.main_ppo \
     --config-path="/dfs/data/work/hardtry/exps/verl1/configs" \
