4e279bfbd1685c62ac78ce78dd0eb92176b2666f
zhangdw <zhangdw.cs@gmail.com>
2026-02-03 17:51:56 +0800

    feat: update exps/lora1/train_local.sh

--- exps/lora1/train_local.sh
+++ exps/lora1/train_local.sh
@@ -5,4 +5,5 @@ source /dfs/data/uv-venv/modelscope/bin/activate
 OMP_NUM_THREADS=4 \
 NPROC_PER_NODE=4 \
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
-    swift sft --config configs/sft_config.yaml
+    swift sft --config \
+        "/dfs/data/work/hardtry/exps/lora1/configs/sft_config.yaml"
