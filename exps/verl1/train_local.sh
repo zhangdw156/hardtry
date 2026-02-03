9dfc2ac5d203ad70c01487c526daa85065f86d74
zhangdw <zhangdw.cs@gmail.com>
2026-02-03 10:41:40 +0800

    fix: fix verl exp

--- exps/verl1/train_local.sh
+++ exps/verl1/train_local.sh
@@ -10,10 +10,10 @@ CONFIG_NAME="grpo_config"
 LOG_PATH="${SCRIPT_DIR}/logs/grpo.log"
 
 export RAY_DISABLE_DASHBOARD=1
-export WORLD_SIZE=$(nvidia-smi -L | wc -l)
-export OMP_NUM_THREADS=4
-export MKL_NUM_THREADS=4
-export VE_OMP_NUM_THREADS=4
+export WORLD_SIZE=4
+export OMP_NUM_THREADS=8
+export MKL_NUM_THREADS=8
+export VE_OMP_NUM_THREADS=8
 
 ${PYTHON_CMD} -m verl.trainer.main_ppo \
     --config-path=${CONFIG_PATH} \
