b771aef29bb3ed3ed18ca3f7b6bc8c2b47846f1d
zhangdw <zhangdw.cs@gmail.com>
2026-02-05 11:12:18 +0800

    feat: add merge fsdp scripts

---
+++ exps/full1/merge_local.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source /dfs/data/uv-venv/huggingface/bin/activate
+
+BASE_MODEL_PATH="/dfs/data/models/Qwen3-4B-Instruct-2507"
+CKPT_PATH="/dfs/data/work/hardtry/checkpoints/full1/v6-20260204-133724/checkpoint-162/pytorch_model_fsdp_0"
+OUTPUT_PATH="/dfs/data/models/hardtry-4b-full1"
+
+accelerate merge-weights \
+    ${CKPT_PATH} \
+    ${OUTPUT_PATH}
+
+cp ${BASE_MODEL_PATH}/tokenizer.json ${OUTPUT_PATH}
+cp ${BASE_MODEL_PATH}/tokenizer_config.json ${OUTPUT_PATH}
+cp ${BASE_MODEL_PATH}/vocab.json ${OUTPUT_PATH}
+cp ${BASE_MODEL_PATH}/merges.txt ${OUTPUT_PATH}
+cp ${BASE_MODEL_PATH}/config.json ${OUTPUT_PATH}
+cp ${BASE_MODEL_PATH}/configuration.json ${OUTPUT_PATH}
+cp ${BASE_MODEL_PATH}/generation_config.json ${OUTPUT_PATH}
