6a110949bab334cee659ae7eb57796cdc703790c
zhangdw <zhangdw.cs@gmail.com>
2026-02-10 20:12:41 +0800

    fix: fix convert_system_content of src/hardtry/utils/convert_messages_to_verl.py

--- src/hardtry/utils/convert_hardgen_to_messages.py
+++ src/hardtry/utils/convert_hardgen_to_messages.py
@@ -102,12 +102,11 @@ def convert_system_content(example):
             if isinstance(tools_obj, dict):
                 tools_obj = [tools_obj]
 
-            # TODO: 这里的tools_header和tools_footer都是错的，或许是导致实验失败的原因！
-            tools_header = "\n\n# Tools\nYou may call one or more functions... <tools>"
+            tools_header = "\n\n# Tools\n\nYou may call one or more functions to assist with the user query.\n\nYou are provided with function signatures within <tools></tools> XML tags:\n<tools>"
             tools_json_body = "".join(
                 ["\n" + json.dumps(t, ensure_ascii=False) for t in tools_obj]
             )
-            tools_footer = "\n</tools>\n\nFor each function call, return a json object within <tool_call></tool_call> tags."
+            tools_footer = "\n</tools>\n\nFor each function call, return a json object with function name and arguments within <tool_call></tool_call> XML tags:\n<tool_call>\n{\"name\": <function-name>, \"arguments\": <args-json-object>}\n</tool_call>"
             tools_text = tools_header + tools_json_body + tools_footer
         except Exception:
             tools_text = ""
