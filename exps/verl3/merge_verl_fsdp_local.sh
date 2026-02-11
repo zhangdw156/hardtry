4b3da88bdf22267b23e5fcf4841bb52abcc71138
zhangdw <zhangdw.cs@gmail.com>
2026-02-11 11:21:39 +0800

    feat: update verl3 exp

---
+++ exps/verl3/merge_verl_fsdp_local.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+##############################
+# TODO: To be modified
+##############################
+LOCAL_DIR="/dfs/data/work/hardtry/checkpoints/verl3/global_step_84/actor"
+##############################
+# TODO: To be modified
+##############################
+TARGET_DIR="/dfs/data/models/hardtry-4b-verl3"
+
+/dfs/data/uv-venv/verl/bin/python3 -m verl.model_merger merge \
+    --backend fsdp \
+    --local_dir ${LOCAL_DIR} \
+    --target_dir ${TARGET_DIR} \
+    --use_cpu_initialization
