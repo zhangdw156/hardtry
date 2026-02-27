0efd697dad6ba01fb0dee4f907025dcb47c08b51
zhangdw <zhangdw.cs@gmail.com>
2026-02-27 14:31:24 +0800

    feat: complete full5 exp

---
+++ exps/full5/train_local.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /dfs/data/uv-venv/modelscope/bin/activate
+
+OMP_NUM_THREADS=4 \
+NPROC_PER_NODE=4 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
+    swift sft --config \
+        "/dfs/data/work/hardtry/exps/full5/configs/sft_config.yaml"
